f1b8b0459c3633b61e32a5c9170b1cea4c08b75c
Dor-J <Mr.DorJosef@gmail.com>
2025-01-19 16:46:09 +0200

    Add createbooks and refactor createdemobooks

--- services/book.service.js
+++ services/book.service.js
@@ -3,7 +3,7 @@ import { storageService } from './async-storage.service.js'
 import { demoBooks } from '../assets/demoData/books.js'
 
 const MISS_BOOKS_KEY = 'MISS_BOOKS_DB'
-_createBooks()
+_createDemoBooks()
 
 export const bookService = {
   query,
@@ -47,11 +47,36 @@ function getDefaultFilter() {
   return { title: '', price: '' }
 }
 
-function _createBooks() {
+function _createDemoBooks() {
   let books = loadFromStorage(MISS_BOOKS_KEY)
   if (!books || !books.length) {
     books = demoBooks
     saveToStorage(MISS_BOOKS_KEY, books)
+  }
+}
+
+function _createBooks() {
+  const ctgs = ['Love', 'Fiction', 'Poetry', 'Computers', 'Religion']
+  const books = []
+  for (let i = 0; i < 20; i++) {
+    const book = {
+      id: utilService.makeId(),
+      title: utilService.makeLorem(2),
+      subtitle: utilService.makeLorem(4),
+      authors: [utilService.makeLorem(1)],
+      publishedDate: utilService.getRandomIntInclusive(1950, 2024),
+      description: utilService.makeLorem(20),
+      pageCount: utilService.getRandomIntInclusive(20, 600),
+      categories: [ctgs[utilService.getRandomIntInclusive(0, ctgs.length - 1)]],
+      thumbnail: `http://coding-academy.org/books-photos/${i + 1}.jpg`,
+      language: 'en',
+      listPrice: {
+        amount: utilService.getRandomIntInclusive(80, 500),
+        currencyCode: 'EUR',
+        isOnSale: Math.random() > 0.7,
+      },
+    }
+    books.push(book)
   }
 }
 
