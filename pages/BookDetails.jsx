b11ec0fce6ae19cf8788ff86c012efb774f57dd4
Dor-J <Mr.DorJosef@gmail.com>
2025-01-19 17:22:16 +0200

    Refactor the BookDetails in pageCount, publishedDate,  price color, nice discount

--- pages/BookDetails.jsx
+++ pages/BookDetails.jsx
@@ -18,8 +18,63 @@ export function BookDetails({ bookId, onBack }) {
       })
   }
 
+  function pageCountText(pageCount) {
+    if (pageCount > 500) {
+      return 'Serious Reading'
+    } else if (pageCount > 200) {
+      return 'Descent Reading'
+    } else if (pageCount > 100) {
+      return ''
+    } else {
+      return 'Light Reading'
+    }
+  }
+  function pageCountHighlight(pageCount) {
+    if (pageCount > 500) {
+      return 'page-count-high'
+    } else if (pageCount > 200) {
+      return 'page-count-medium'
+    } else if (pageCount > 100) {
+      return 'page-count-low'
+    } else {
+      return ''
+    }
+  }
+  function publishedDateText(publishedDate) {
+    const currYear = new Date().getFullYear()
+    if (currYear - publishedDate >= 10) {
+      return 'Vintage'
+    } else if (currYear - publishedDate <= 1) {
+      return 'New'
+    } else return ''
+  }
+
+  function publishedDateHighlight(publishedDate) {
+    const currYear = new Date().getFullYear()
+    if (currYear - publishedDate >= 10) {
+      return 'page-count-high'
+    } else if (currYear - publishedDate <= 1) {
+      return 'page-count-medium'
+    } else return ''
+  }
+  function priceHighlight(price) {
+    if (price > 150) {
+      return 'color-red'
+    } else if (price < 20) {
+      return 'color-green'
+    } else return ''
+  }
+
   if (!book) return <div>Loading...</div>
-  const { title, listPrice, thumbnail, description, authors } = book
+  const {
+    title,
+    listPrice,
+    thumbnail,
+    description,
+    authors,
+    pageCount,
+    publishedDate,
+  } = book
 
   return (
     <section className='book-details'>
@@ -32,12 +87,32 @@ export function BookDetails({ bookId, onBack }) {
             : `Autor: ${authors}`}
         </h2>
         <h2>
+          Number of Pages: {pageCount}{' '}
+          <span className={`text-underline ${pageCountHighlight(pageCount)}`}>
+            {pageCountText(pageCount)}
+          </span>
+        </h2>
+        <h2>
+          Publihed Year: {publishedDate}{' '}
+          <span
+            className={`text-underline ${publishedDateHighlight(
+              publishedDate
+            )}`}
+          >
+            {publishedDateText(publishedDate)}
+          </span>
+        </h2>
+        <h2>
           Book Price:{' '}
           {!listPrice.isOnSale ? (
             listPrice.amount
           ) : (
             <React.Fragment>
-              <span className='text-line-through'>{`${listPrice.amount}`}</span>
+              <span
+                className={`text-line-through ${priceHighlight(
+                  listPrice.amount
+                )}`}
+              >{`${listPrice.amount}`}</span>
               ,
               <span className='text-undeline'>
                 {' Discount! '}
